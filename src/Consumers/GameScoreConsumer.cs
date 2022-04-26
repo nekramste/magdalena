f7426869aed4e79931f0a7f6d23b542ddc7af66e
nelsonb <n@ffsvrs.lv>
2022-04-26 13:50:59 -0400

    Register StringEnumConverter

--- src/Consumers/GameScoreConsumer.cs
+++ src/Consumers/GameScoreConsumer.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace FF.Magdalena.Consumers
