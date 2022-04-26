ec3024806469036e89299ddb0dde83285b24d689
oreamunob19 <oreamunob19@gmail.com>
2022-04-26 10:44:45 -0600

    notify init

--- src/ClientApp/store/index.js
+++ src/ClientApp/store/index.js
@@ -3,6 +3,20 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+function replacePort(host){
+  return host? host.split(':')[0]+':44328':'';
+}
+
+async function notifyInit(id){
+  try {     
+    var protocol = "https:";
+    var uri = `${protocol}//${replacePort(window.location.host)}/connection/ready?connectionId=${id}`;    
+    fetch(uri).then(response => response.json())
+  } catch (err) {
+    console.log(err)
+  }
+}
+
 const SCORES = 'SCORES'
 
 const state = {
@@ -11,10 +25,10 @@ const state = {
 }
 
 const mutations = {
-  [SCORES](state, obj) {   
+  [SCORES](state, obj) {
 
     let data = obj.score.includes('{')?(JSON.parse(obj.score)):obj.score;
-    let score = obj.score.includes('{')?(JSON.parse(obj.score)):null;    
+    let score = obj.score.includes('{')?(JSON.parse(obj.score)):null;
 
     // ALL
     if (score) {
@@ -26,6 +40,7 @@ const mutations = {
       }      
     }else{ // FIRST CONNECTION - GET ID
       state.id = data.split(' ')[0];
+      notifyInit(state.id)
       console.log(state.id);
     }    
 
