ae1b6611a456c1fdd64b5b1221e5ea2a25b8c5f0
nelsonb <n@ffsvrs.lv>
2022-04-28 11:42:26 -0400

    Use proper routes

--- src/ClientApp/src/common/config.js
+++ src/ClientApp/src/common/config.js
@@ -1,10 +1,10 @@
 const isProduction = 0;
 
 const API_ROUTE_DEVELOPMENT ='http://localhost:5000';
-const API_ROUTE_PRODUCTION = 'https://i.preprod.youwager.lv/webs';
+const API_ROUTE_PRODUCTION = 'https://agadmin';
 
 const WS_ROUTE_DEVELOPMENT ='ws://localhost:50000';
-const WS_ROUTE_PRODUCTION = 'https://i.preprod.youwager.lv/webs';
+const WS_ROUTE_PRODUCTION = 'https://agadmin';
 
 export default {
     API_URL: isProduction? API_ROUTE_PRODUCTION : API_ROUTE_DEVELOPMENT,
